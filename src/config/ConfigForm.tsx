99b92c4d00e9f3c3dba9a9dbc11ce16604387b27
okayus <toshiaki.mukai.9981@gmail.com>
2025-03-21 15:17:16 +0900

    fix:configForm

--- src/config/ConfigForm.tsx
+++ src/config/ConfigForm.tsx
@@ -11,9 +11,8 @@ import { KintoneSdk } from "../shared/util/kintoneSdk";
 import { KintoneUtil } from "../shared/util/KintoneUtil";
 
 import type { ConfigSchema } from "../shared/types/Config";
-import type { Properties } from "@kintone/rest-api-client/lib/src/client/types";
 import type { IChangeEvent } from "@rjsf/core";
-import type { RJSFSchema, UiSchema } from "@rjsf/utils";
+import type { RJSFSchema } from "@rjsf/utils";
 
 interface AppProps {
   pluginId: string;
@@ -21,49 +20,18 @@ interface AppProps {
   kintoneUtil: typeof KintoneUtil;
 }
 
-interface FieldOption {
-  const: string;
-  title: string;
-}
-
 const log = (type: string) => console.log.bind(console, type);
-
-const generateFieldOptions = (
-  properties: Properties,
-  fields: string[],
-): FieldOption[] => {
-  const options = Object.keys(properties)
-    .filter((fieldCode) => fields.includes(properties[fieldCode].type))
-    .map((fieldCode) => {
-      return {
-        const: fieldCode,
-        title: properties[fieldCode].label,
-      };
-    });
-  options.unshift({ const: "", title: "" });
-  return options;
-};
 
 const ConfigForm: React.FC<AppProps> = ({
   pluginId,
   kintoneSdk,
   kintoneUtil,
 }) => {
-  const [fieldOptions, setFieldOptions] = useState<FieldOption[]>([]);
   const [formData, setFormData] = useState<ConfigSchema>({} as ConfigSchema);
 
   useEffect(() => {
     const fetchApps = async () => {
       try {
-        const response = await kintoneSdk.getFormFields(
-          Number(kintoneUtil.getId()),
-        );
-
-        const itemOptions = generateFieldOptions(response, [
-          "SINGLE_LINE_TEXT",
-        ]);
-        setFieldOptions(itemOptions);
-
         const responseConfig = kintoneUtil.getConfig(pluginId);
         if (responseConfig.config) {
           setFormData(JSON.parse(responseConfig.config).config);
@@ -140,29 +108,12 @@ const ConfigForm: React.FC<AppProps> = ({
 
   const dynamicSchema = {
     ...configSchema,
-    properties: {
-      ...configSchema.properties,
-      fields: {
-        type: "array",
-        items: {
-          type: "string",
-          enum: fieldOptions.map((option) => option.const),
-        },
-      },
-    },
-  };
-
-  const UiSchema: UiSchema = {
-    prefix: {
-      "ui:widget": "textarea",
-    },
   };
 
   return (
     <div>
       <Form
         schema={dynamicSchema as RJSFSchema}
-        uiSchema={UiSchema}
         validator={validator}
         formData={formData}
         onSubmit={handleSubmit}
